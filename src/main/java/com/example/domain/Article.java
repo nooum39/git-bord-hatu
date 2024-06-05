251b3db8e7fe2c0a88d53bdc6fcda9e23bf03b0c
nooum39 <mikantaruto1380@gmail.com>
2024-06-05 13:32:53 +0900

    first commit

---
+++ src/main/java/com/example/domain/Article.java
@@ -0,0 +1,74 @@
+package com.example.domain;
+
+import java.util.List;
+
+public class Article {
+
+    /**
+     * 記事情報を表すドメインクラス.
+     *
+     * @author haruka.yamaneki
+     */
+    public class SeparatedArticle {
+
+        /**
+         * 記事番号
+         */
+        private Integer id;
+
+        /**
+         * 投稿者名
+         */
+        private String name;
+
+        /**
+         * 投稿内容
+         */
+        private String content;
+
+        /**
+         * 記事についたコメントリスト
+         */
+        private List<Comment> commentList;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getContent() {
+            return content;
+        }
+
+        public void setContent(String content) {
+            this.content = content;
+        }
+
+        public List<Comment> getCommentList() {
+            return commentList;
+        }
+
+        public void setCommentList(List<Comment> commentList) {
+            this.commentList = commentList;
+        }
+
+        @Override
+        public String toString() {
+            return "SeparatedArticle [id=" + id + ", name=" + name + ", content=" + content + ", commentList=" + commentList
+                    + "]";
+        }
+    }
+}
+
+
